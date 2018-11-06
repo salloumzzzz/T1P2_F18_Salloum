081b87074791a34e4d21fd6d93396ee989d7fda1
salloumzzz <georges.salloum@std.balamand.edu.lb>
2018-11-06 14:03:11 +0200

    creationg a script

--- script1.sh
+++ script1.sh
@@ -1,7 +1,13 @@
  #!/bin/bash
-echo "Please enter any sentence"
+echo "enter a regular expression"
 read foo
-var=$(echo $foo | tr "{a-z}" "{A-Z}")
+
+var=$(echo $foo | tr "{a-z}" "{A-Z} ")
+
+
+
+var=$(echo $foo | tr "{I}" "{A} ")
+
  # {a-z} Matches a through z
  # {A-Z} matches A through Z
 echo $var
